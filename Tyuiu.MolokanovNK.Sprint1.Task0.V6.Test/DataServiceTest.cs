72582a5a738476f089153138b3af403cf7367163
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-09-20 11:14:55 +0500

    Add project files.

---
+++ Tyuiu.MolokanovNK.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MolokanovNK.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint1.Task0.V6/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MolokanovNK.Sprint1.Task0.V6.Lib;
+namespace Tyuiu.MolokanovNK.Sprint1.Task0.V6
+{
+   class programm
+   {
+       static void Main(string[] args)
+       {
+           DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил : Молоканов Н. К. | ИБКСб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значение двух    *");
+            Console.WriteLine("* Одинаковых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 3 * (4 + 1)                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+   }
+}
