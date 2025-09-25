384eed0f34e2c15e68b5f66459ac49670de22041
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-09-25 23:42:15 +0500

    Task3

---
+++ Tyuiu.MolokanovNK.Sprint1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MolokanovNK.Sprint1.V3.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 9;
+            double y = 7.5;
+            double z = 5;
+            var res = ds.ParallelepipedVolume(x, y, z );
+            Assert.AreEqual(337.5, res);
+        }
+    }
+}
