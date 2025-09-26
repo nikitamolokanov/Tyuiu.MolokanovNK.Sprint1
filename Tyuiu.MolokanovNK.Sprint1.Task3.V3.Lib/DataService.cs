f384905f94596ea225192dac542633c677d873e8
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-09-26 20:17:38 +0500

    Task3

---
+++ Tyuiu.MolokanovNK.Sprint1.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MolokanovNK.Sprint1.Task3.V3.Lib
+{
+    public class DataService : ISprint1Task3V3
+    {
+        public double ParallelepipedVolume(double length, double width, double height)
+        {
+            return length * width * height; 
+        }
+    }
+}
