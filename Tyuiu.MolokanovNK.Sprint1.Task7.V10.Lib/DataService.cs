f97f622cce614d7457e9ef8660f04b77f861a58a
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-09-29 22:11:57 +0500

    Task7

---
+++ Tyuiu.MolokanovNK.Sprint1.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MolokanovNK.Sprint1.Task7.V10.Lib
+{
+    public class DataService : ISprint1Task7V10
+    {
+        public double Calculate(double x)
+        {
+            return Math.Round(  2 * (Math.Cos(3 * x) / Math.Sin(3 * x)) - (Math.Log(Math.Cos(x)) / Math.Log(1 + x * x)),3);
+
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MolokanovNK.Sprint1.Task7.V10.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(-13.142, res);
+        }
+    }
+}
